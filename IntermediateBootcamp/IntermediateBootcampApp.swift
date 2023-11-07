b881f7cb2f23c3020719fea94566182672420fff
drgndenis <serkis-denis.dragan@ogr.iuc.edu.tr>
2023-11-07 20:04:22 +0300

    Initial Commit

---
+++ IntermediateBootcamp/IntermediateBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntermediateBootcampApp.swift
+//  IntermediateBootcamp
+//
+//  Created by Denis DRAGAN on 7.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct IntermediateBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
